8827fc8fb98f2920acbde379351296011ffdc4a4
unknown <caio cesar>
2024-11-26 23:22:28 -0300

    Algumas alterações no js

--- index.js
+++ index.js
@@ -9,11 +9,47 @@ const h = canvas.height = 576;
 let mapWidth = -200;
 let mapHeight = -630;
 
+
 ctx.fillStyle = "white";
 ctx.fillRect(0,0, w, h);
 
-player.onload = () => {
-    ctx.drawImage(map, mapWidth,  mapHeight);
+class Sprite {
+    constructor(position, map){
+        this.position = position
+        this.map = map
+    }
+    draw(){
+        ctx.drawImage(map, -200, -630)
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -200,
+        y: -630
+    },
+    map: map
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+
+}
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    background.draw()
     ctx.drawImage(
         player,
         0,
@@ -24,5 +60,44 @@ player.onload = () => {
         h/2 + player.height,
         player.width/6,
         player.height
-    );
+    )
+    if (keys.w.pressed){
+        background.position.y = background.position.y + 3
+    }
+    
 }
+animate()
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key){
+        case 'w':
+            keys.w.pressed = true
+            break;
+        case 'a':
+            keys.a.pressed = true
+            break;
+        case 's':
+            keys.s.pressed = true
+            break;
+        case 'd':
+            keys.d.pressed = true
+            break;
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key){
+        case 'w':
+            keys.w.pressed = false
+            break;
+        case 'a':
+            keys.a.pressed = false
+            break;
+        case 's':
+            keys.s.pressed = false
+            break;
+        case 'd':
+            keys.d.pressed = false
+            break;
+    }
+})
